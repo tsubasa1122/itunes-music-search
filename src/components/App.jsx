3276fb984e0a046035bc1495e02332a9eefe7005
tsubasa1122 <nakanot@open8.com>
2020-04-26 00:13:32 +0900

    [add]css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,14 @@ import React from "react";
 
 export default class App extends React.Component {
   render() {
-    return <h1>Hello World in React</h1>;
+    return (
+      <div>
+        <div className="search-area">
+          <input type="text" className="search-input" />
+          <buttton className="search-button">検索</buttton>
+        </div>
+    </div>
+    )
+    ;
   }
 }
