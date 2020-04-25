32201442c8e94c4e73e3ebe102ce5e8a5749ef2f
tsubasa1122 <nakanot@open8.com>
2020-04-26 00:30:03 +0900

    [add]検索フォーム

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,23 @@
 import React from "react";
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      keyword: ""
+    };
+  }
+
+  handleInputChange(e) {
+    this.setState({
+      keyword: e.target.value
+    });
+  }
   render() {
     return (
       <div>
         <div className="search-area">
-          <input type="text" className="search-input" />
+          <input type="text" className="search-input" value={this.state.keyword} onChange={this.handleInputChange.bind(this)} />
           <buttton className="search-button">検索</buttton>
         </div>
     </div>
